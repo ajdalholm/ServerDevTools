036b9e8bc66766bf30e7f1ca7d415e56bcd50ed7
Anders Jarl Dalholm <dalholm@gmail.com>
2024-12-06 09:30:11 +0100

    slimmed down

--- Installer.ps1
+++ Installer.ps1
@@ -9,21 +9,7 @@
 #>
 
 Install-Script Winget-Install
-Winget-Install
-
-Push-Location -Path $Tempfolder.FullName -ErrorAction SilentlyContinue
-Add-AppxPackage -Path .\Microsoft.UI.Xaml.2.8.appx
-Add-AppxPackage -Path .\Microsoft.VCLibs.x64.14.00.Desktop.appx
-#Join winget package from parts
-$firstPartOfFile = Get-Item -path .\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle.part1
-$tempFile = $firstPartOfFile.Name.Substring(0,$firstPartOfFile.Name.LastIndexOf('.'))
-New-Item -Path . -Name $tempFile -ItemType File
-Join-File -infilePrefix $firstPartOfFile.FullName.Substring(0,$firstPartOfFile.FullName.Length-1) -outFilePath (Join-Path -Path $firstPartOfFile.Directory.FullName -ChildPath $tempFile)
-Start-Sleep -Seconds 3
-Add-AppxProvisionedPackage -Online -PackagePath $tempFile -LicensePath .\76fba573f02545629706ab99170237bc_License1.xml
-Pop-Location
-
-Start-Sleep -Seconds 5
+Winget-Install.ps1
 
 winget install Git.Git 7zip.7zip Google.Chrome Microsoft.VisualStudioCode Microsoft.PowerShell WinMerge.WinMerge Microsoft.WindowsTerminal --accept-source-agreements --accept-package-agreements --scope machine
 
